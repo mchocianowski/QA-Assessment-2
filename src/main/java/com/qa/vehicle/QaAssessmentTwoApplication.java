1a74a8f2544ba91d98c98c4b0f3d86b945719e6d
Mateusz Chocianowski <ch.mateusz@hotmail.com>
2022-04-15 14:11:53 +0100

    QAHP-26 QAHP-27 #comment initial commit

---
+++ src/main/java/com/qa/vehicle/QaAssessmentTwoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaAssessmentTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaAssessmentTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/vehicle/QaAssessmentTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaAssessmentTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
