cac89cfc47f1f56b032431c5db93114f91bec3c9
Mateusz Chocianowski <ch.mateusz@hotmail.com>
2022-04-19 11:23:19 +0100

    QAHP-41 #comment Create a domain package and vehicle class inside it

---
+++ src/main/java/com/qa/vehicle/domain/vehicle.java
@@ -0,0 +1,17 @@
+package com.qa.vehicle.domain;
+
+
+public class vehicle{
+	
+	private Integer id;
+
+	private String brand;
+	
+	private String model;
+	
+	private Integer price;
+	
+	private Integer mileage;
+	
+	
+}
