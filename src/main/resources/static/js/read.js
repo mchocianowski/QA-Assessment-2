bb5b12d6171502736856c6c44c3946a890006e51
Mateusz Chocianowski <ch.mateusz@hotmail.com>
2022-04-20 12:55:36 +0100

    QAHP-54 QAHP-53 #comment Add read by id functionality to front end

--- src/main/resources/static/js/read.js
+++ src/main/resources/static/js/read.js
@@ -1,27 +1,44 @@
 `use strict`;
-
+console.log("File connected");
 // import the dom
 import * as DOM from "./dom.js";
 
+// console.log(DOM.buttonViewAll);
+console.log("File connected");
+
 // list item function
 const writeVItem = (vitem) => {
-  const child = document.createElement(`li`);
-  child.id = vitem._id;
-  child.innerHTML = `${JSON.stringify(vitem)}`;
-  DOM.listOutput.appendChild(child);
+  const listbody = document.createElement(`div`);
+  const brand = document.createElement(`li`);
+  const model = document.createElement(`li`);
+  const price = document.createElement(`li`);
+  const mileage = document.createElement(`li`);
+  listbody.id = vitem._id;
+
+  brand.innerHTML = `Brand: ${vitem.brand}`
+  model.innerHTML = `Model: ${vitem.model}`
+  price.innerHTML = `Price: ${vitem.price}`
+  model.innerHTML = `Mileage: ${vitem.mileage}`
+  listbody.appendChild(brand);
+  listbody.appendChild(model);
+  listbody.appendChild(price);
+  listbody.appendChild(mileage);
+  DOM.listOutput.appendChild(listbody);
 };
 
 // GET all function
 const get = () => {
+  console.log(DOM.listOutput);
   DOM.listOutput.innerHTML = ``;
 
   axios
-    .get(`http://localhost8080/getAll`)
+    .get(`http://localhost:8080/getAll`)
     .then((response) => {
       if (!Array.isArray(response.data)) {
         writeVItem(response.data);
       } else {
         for (let vitem of response.data) {
+          console.log(vitem);
           writeVItem(vitem);
         }
       }
@@ -34,14 +51,13 @@ const get = () => {
 // GET By Id function
 const getById = () => {
   let id = DOM.readId.value;
+  console.log(id);
   let output = DOM.listOutput;
   axios
-    .get(`http://localhost8080/readById/` + id)
+    .get(`http://localhost:8080/get/${id}`)
     .then((response) => {
       output.innerHTML = ` `;
-      for (let vitem of response.data) {
-        writeVItem(vitem);
-      }
+        writeVItem(response.data);
     })
     .catch((err) => {
       console.log(err);
@@ -53,7 +69,7 @@ const getByBrand = () => {
   let brand = DOM.readBrand.value;
   let output = DOM.listOutput;
   axios
-    .get(`http://localhost8080/readByBrand/` + brand)
+    .get(`http://localhost:8080/readByBrand/` + brand)
     .then((response) => {
       output.innerHTML = ` `;
       for (let vitem of response.data) {
@@ -70,7 +86,7 @@ const getByPrice = () => {
   let brand = DOM.readPrice.value;
   let output = DOM.listOutput;
   axios
-    .get(`http://localhost8080/readByPrice/` + price)
+    .get(`http://localhost:8080/readByPrice/` + price)
     .then((response) => {
       output.innerHTML = ` `;
       for (let vitem of response.data) {
@@ -87,7 +103,7 @@ const getByMileage = () => {
   let brand = DOM.readMileage.value;
   let output = DOM.listOutput;
   axios
-    .get(`http://localhost8080/readByMileage/` + mileage)
+    .get(`http://localhost:8080/readByMileage/` + mileage)
     .then((response) => {
       output.innerHTML = ` `;
       for (let vitem of response.data) {
@@ -102,8 +118,8 @@ const getByMileage = () => {
 // set up the buttons' on click events
 DOM.buttonViewAll.onclick = () => get();
 DOM.buttonReadById.onclick = () => getById();
-DOM.buttonReadByBrand.onclick = () => getByBrand();
-DOM.buttonReadByBrand.onclick = () => getByPrice();
-DOM.buttonReadByBrand.onclick = () => getByMileage();
-DOM.buttonCreate.onclick = () => post();
-DOM.buttonDelete.onclick = () => deleteById();
+// DOM.buttonReadByBrand.onclick = () => getByBrand();
+// DOM.buttonReadByPrice.onclick = () => getByPrice();
+// DOM.buttonReadByMileage.onclick = () => getByMileage();
+// DOM.buttonCreate.onclick = () => post();
+// DOM.buttonDelete.onclick = () => deleteById();
